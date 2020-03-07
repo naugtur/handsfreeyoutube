2cf86875318317ab3a30d79380297adde37f8316
Naugtur <naugtur@gmail.com>
2020-03-08 00:41:31 +0100

    playing works

--- api/yt.ts
+++ api/yt.ts
@@ -1,26 +1,14 @@
 import ytdl from 'ytdl-core'
-import { safeMemoryCache } from 'safe-memory-cache'
 import { NowRequest, NowResponse } from '@now/node'
 
-const cache = safeMemoryCache({
-    limit: 100
-})
 
 function getVidInfo(vid) {
-    let ytInfo = cache.get(vid)
-    if (!ytInfo) {
-        console.log(`populating cache for ${vid}`)
-        ytInfo = ytdl.getInfo(`https://www.youtube.com/watch?v=${vid}`)
-            .then(info => {
-                const formats = ytdl.filterFormats(info.formats, 'audioonly')
-                const prefered = formats.filter(f => f.container.match(/mp4|m4a/))[0]
-                return prefered || formats[0]
-            })
-        cache.set(vid, ytInfo)
-    } else {
-        console.log(`using cache for ${vid}`)
-    }
-    return ytInfo
+    return ytdl.getInfo(`https://www.youtube.com/watch?v=${vid}`)
+        .then(info => {
+            const formats = ytdl.filterFormats(info.formats, 'audioonly')
+            const prefered = formats.filter(f => f.container.match(/mp4|m4a/))[0]
+            return prefered || formats[0]
+        })
 }
 
 export default (req: NowRequest, res: NowResponse) => {
